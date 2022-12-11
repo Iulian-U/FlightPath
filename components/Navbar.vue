45dadfec20456f09b425d40cbf471efc5be76c4e
Iulian Ursu <59142419+Iulian-U@users.noreply.github.com>
2022-12-11 17:05:09 +0200

    Create Navbar.vue

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <a class="nav-logo">FlightPath ✈️</a>
+    <a href="">About</a>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style scoped>
+nav {
+  @apply flex justify-between items-center px-2 bg-white h-16 shadow-sm;
+}
+
+nav a {
+  @apply px-2;
+}
+</style>

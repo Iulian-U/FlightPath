df0de874e846cd055b812a4f1861e0e649822dcc
Iulian Ursu <59142419+Iulian-U@users.noreply.github.com>
2022-12-11 17:05:13 +0200

    Create FlightTracker.vue

---
+++ components/FlightTracker.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <input type="text" class="rounded-lg bg-white py-2 pl-3 pr-10" />
+    <button>Track Flight</button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>

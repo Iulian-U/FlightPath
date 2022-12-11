9f70d65cbf81bafb25f438ae6b08c881ec600ce0
Iulian Ursu <59142419+Iulian-U@users.noreply.github.com>
2022-12-11 17:05:37 +0200

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <div class="content">
+      <FlightTracker />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+#app {
+  @apply w-full h-full flex flex-col;
+}
+
+.content {
+  @apply w-full h-full flex justify-center items-center;
+}
+</style>

e9484e19a4edd96bc42beb66f22ffc6034d8a257
Iulian Ursu <59142419+Iulian-U@users.noreply.github.com>
2022-12-16 13:42:34 +0200

    moved the logo into own component

---
+++ components/Design/Logo.vue
@@ -0,0 +1,20 @@
+<template>
+  <a>
+    <img src="@/assets/images/paper-plane.png" alt="logo" />
+    <p>FlightPath</p>
+  </a>
+</template>
+
+<style scoped>
+a {
+  @apply flex items-center  bg-white p-4 rounded-md shadow-sm h-12;
+}
+
+p {
+  @apply ml-1 text-2xl antialiased font-bold tracking-normal text-sky-500;
+}
+
+img {
+  @apply h-10 rounded-md;
+}
+</style>

3f55509a8140eeb89141fe3f003fff204ebe4c19
Iulian Ursu <59142419+Iulian-U@users.noreply.github.com>
2022-12-16 13:42:53 +0200

    included new components

---
+++ components/Main/Navbar.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <nav>
+    <DesignLogo />
+    <MainSettings />
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  @apply flex justify-between items-center px-2 h-20;
+}
+</style>

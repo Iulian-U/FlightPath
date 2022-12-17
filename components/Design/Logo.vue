edfac59d172960d37a4649ad040df45b0c54f59a
Iulian Ursu <59142419+Iulian-U@users.noreply.github.com>
2022-12-17 16:03:13 +0200

    updated logo color based on page theme

--- components/Design/Logo.vue
+++ components/Design/Logo.vue
@@ -1,17 +1,25 @@
+<script setup>
+import logoColor from '/images/plane-logo-color.png'
+import logoWhite from '/images/plane-logo-white.png'
+const colorMode = useColorMode();
+
+
+</script>
+
 <template>
   <a>
-    <img src="@/assets/images/paper-plane.png" alt="logo" />
+    <img :src="colorMode.preference == 'light' ? logoColor : logoWhite" alt="logo" />
     <p>FlightPath</p>
   </a>
 </template>
 
 <style scoped>
 a {
-  @apply flex items-center  bg-white p-4 rounded-md shadow-sm h-12;
+  @apply flex items-center bg-white dark: bg-blue-600 p-4 rounded-md shadow-sm h-12;
 }
 
 p {
-  @apply ml-1 text-2xl antialiased font-bold tracking-normal text-sky-500;
+  @apply ml-1 text-2xl antialiased font-bold tracking-normal text-sky-500 dark: text-white;
 }
 
 img {

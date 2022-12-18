24542456199935c65a9dd904630a37c25cdf8eb2
Iulian Ursu <59142419+Iulian-U@users.noreply.github.com>
2022-12-18 10:29:02 +0200

    included animations.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/color-mode", "nuxt-windicss"],
-  css: ["@/assets/css/global.css"],
+  css: ["@/assets/css/global.css", "@/assets/css/animations.css"],
   colorMode: {
     classSuffix: "",
   },

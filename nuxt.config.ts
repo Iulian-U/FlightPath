805ff9a05c8ecd407f69ccf998511a35bf39923c
Iulian Ursu <59142419+Iulian-U@users.noreply.github.com>
2022-12-28 14:51:14 +0200

    added Axios & Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/color-mode", "nuxt-windicss"],
+  imports: {
+    dirs: ["stores"],
+  },
+  modules: [
+    "@nuxtjs/color-mode",
+    "nuxt-windicss",
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: ["defineStore", "acceptHMRUpdate"],
+      },
+    ],
+  ],
+
   css: ["@/assets/css/global.css", "@/assets/css/animations.css"],
   colorMode: {
     classSuffix: "",
